cc56532efd2a0a0f9a7d009c3612ccc59714e969
Rafal Stachura <rafstach.97@gmail.com>
2020-11-23 21:11:37 +0100

    config update

--- app/src/main/java/agh/edu/pl/facelivenessdetection/model/method1/Method.java
+++ app/src/main/java/agh/edu/pl/facelivenessdetection/model/method1/Method.java
@@ -4,18 +4,18 @@ import android.graphics.Bitmap;
 
 import agh.edu.pl.facelivenessdetection.model.MobileModel;
 
-public class Method extends Thread{
+public class Method extends Thread {
 
     MobileModel mobileModel;
 
-    public Method(MobileModel mobileModel){
+    public Method(MobileModel mobileModel) {
         this.mobileModel = mobileModel;
     }
 
     @Override
     public void run() {
         Bitmap m = mobileModel.getPhoto();
-        System.out.println(m.getColor(0,0));
+        System.out.println(m.getColor(0, 0));
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
