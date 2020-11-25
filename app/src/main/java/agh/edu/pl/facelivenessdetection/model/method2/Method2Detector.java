6bbe258b0cc8d42409da919118d86a500a78a50d
Rafal Stachura <rafstach.97@gmail.com>
2020-11-25 20:50:35 +0100

    view layout refactor

--- app/src/main/java/agh/edu/pl/facelivenessdetection/model/method2/Method2Detector.java
+++ app/src/main/java/agh/edu/pl/facelivenessdetection/model/method2/Method2Detector.java
@@ -2,11 +2,12 @@ package agh.edu.pl.facelivenessdetection.model.method2;
 
 import agh.edu.pl.facelivenessdetection.model.LivenessDetector;
 import agh.edu.pl.facelivenessdetection.model.MobileModel;
+import agh.edu.pl.facelivenessdetection.model.method1.Method;
 
-public class Method2Detector implements LivenessDetector {
+public class  Method2Detector implements LivenessDetector {
 
     @Override
     public Runnable detect(MobileModel m) {
-        return null;
+        return new Method(m);
     }
 }
