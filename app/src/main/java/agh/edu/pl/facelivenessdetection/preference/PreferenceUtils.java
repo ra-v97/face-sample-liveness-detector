666225e54f0d1d261aa5505657d97c1179a6befe
Rafal Stachura <rafstach.97@gmail.com>
2020-11-26 11:59:49 +0100

    add part of camera handling library

---
+++ app/src/main/java/agh/edu/pl/facelivenessdetection/preference/PreferenceUtils.java
@@ -0,0 +1,47 @@
+package agh.edu.pl.facelivenessdetection.preference;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import androidx.annotation.Nullable;
+import com.google.android.gms.common.images.Size;
+import com.google.common.base.Preconditions;
+
+import agh.edu.pl.facelivenessdetection.R;
+import agh.edu.pl.facelivenessdetection.camera.CameraSource;
+
+public class PreferenceUtils {
+
+    @Nullable
+    public static CameraSource.SizePair getCameraPreviewSizePair(Context context, int cameraId) {
+        Preconditions.checkArgument(
+                cameraId == CameraSource.CAMERA_FACING_BACK
+                        || cameraId == CameraSource.CAMERA_FACING_FRONT);
+        String previewSizePrefKey;
+        String pictureSizePrefKey;
+        if (cameraId == CameraSource.CAMERA_FACING_BACK) {
+            previewSizePrefKey = context.getString(R.string.pref_key_rear_camera_preview_size);
+            pictureSizePrefKey = context.getString(R.string.pref_key_rear_camera_picture_size);
+        } else {
+            previewSizePrefKey = context.getString(R.string.pref_key_front_camera_preview_size);
+            pictureSizePrefKey = context.getString(R.string.pref_key_front_camera_picture_size);
+        }
+
+        try {
+            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+            return new CameraSource.SizePair(
+                    Size.parseSize(sharedPreferences.getString(previewSizePrefKey, null)),
+                    Size.parseSize(sharedPreferences.getString(pictureSizePrefKey, null)));
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public static boolean isCameraLiveViewportEnabled(Context context) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        String prefKey = context.getString(R.string.pref_key_camera_live_viewport);
+        return sharedPreferences.getBoolean(prefKey, false);
+    }
+
+    private PreferenceUtils() {}
+}
